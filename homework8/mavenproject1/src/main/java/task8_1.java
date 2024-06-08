25ce5607803c7992bb5c8439179599a47865b66e
SKRID_ONE <goodfriend483@gmail.com>
2024-06-08 12:45:53 +0300

    8 hm master

--- homework8/mavenproject1/src/main/java/task8_1.java
+++ homework8/mavenproject1/src/main/java/task8_1.java
@@ -1,13 +1,15 @@
 /**
- *
+ *  В NeatBeans IDE maven и gradle проекты похоже имеют отличную от вашей структуры.
  * @author Спирин Кирилл
  */
 public class task8_1 {
 
         public static void main(String[] args) {
-        
-            System.out.println("Создано в ветке мастер");
-            
+            voice();
         }
     
+        
+        public static void voice () {
+            System.out.println("Создано в ветке мастер");
+        }
 }
