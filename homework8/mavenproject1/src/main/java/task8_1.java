c9bed2e4e648c3404e916b4eb899415c12f7ab65
SKRID_ONE <goodfriend483@gmail.com>
2024-06-08 12:34:27 +0300

    homework 8 - master

---
+++ homework8/mavenproject1/src/main/java/task8_1.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Спирин Кирилл
+ */
+public class task8_1 {
+
+        public static void main(String[] args) {
+        
+            System.out.println("Создано в ветке мастер");
+            
+        }
+    
+}
